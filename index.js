370675f68df16dc9cd3e156edbe3741aade34904
Samir Singh <samir@antino.com>
2025-09-15 22:31:05 +0530

    revise call, apply and bind with its polyfill

--- index.js
+++ index.js
@@ -1,70 +1,57 @@
-const tabConfig = [
-  {
-    id: 1,
-    title: "Tab 1",
-    content: "<h1>Tab One Heading</h1> <p>Tab one paragraph</p>",
-  },
-  {
-    id: 2,
-    title: "Tab 2",
-    content: "<h1>Tab Two Heading</h1> <p>Tab two paragraph</p>",
-  },
-  {
-    id: 3,
-    title: "Tab 3",
-    content: "<h1>Tab Three Heading</h1> <p>Tab three paragraph</p>",
-  },
-];
+let obj = {
+  name: "John",
+};
 
-const tabContainer = document.querySelector(".tab-container");
-const tabHead = document.querySelector(".tab-head");
-const tabBody = document.querySelector(".tab-body");
-
-let activeId = String(tabConfig[0].id);
-
-tabConfig.forEach((item) => {
-  const tabButton = document.createElement("button");
-  tabButton.innerText = item.title;
-  tabButton.setAttribute("data-id", item.id);
-  tabButton.classList.add("tab-button");
-
-  tabHead.appendChild(tabButton);
-
-  const tabContent = document.createElement("div");
-  tabContent.setAttribute("data-id", item.id);
-  tabContent.classList.add("tab-content");
-  tabContent.innerHTML = item.content;
-
-  tabBody.append(tabContent);
-});
-
-activeTab(activeId);
-
-tabContainer.addEventListener("click", function (e) {
-  if (e.target.nodeName === "BUTTON") {
-    activeId = e.target.getAttribute("data-id");
-    activeTab(activeId);
-  }
-});
-
-function activeTab(id) {
-  const tabButtons = document.querySelectorAll(".tab-button");
-  tabButtons.forEach((item) => {
-    if (item.getAttribute("data-id") === id) {
-      //button active
-      item.classList.add("active-button");
-    } else {
-      // button not active
-      item.classList.remove("active-button");
-    }
-  });
-
-  const tabContents = document.querySelectorAll(".tab-content");
-  tabContents.forEach((item) => {
-    if (item.getAttribute("data-id") === id) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
+function printName(age, city) {
+  console.log(`Hello my name is ${this.name} ${age} yrs old from ${city}`);
 }
+
+// Call Polyfill
+Function.prototype.myCall = function (context = {}, ...args) {
+  if (typeof this !== "function") {
+    throw new Error(`${this} should be a function`);
+  }
+
+  context.fn = this;
+
+  context.fn(...args);
+
+  delete context.fn;
+};
+
+// Apply Polyfill
+Function.prototype.myApply = function (context = {}, args = []) {
+  if (typeof this !== "function") {
+    throw new Error(`${this} should be a function`);
+  }
+
+  if (!Array.isArray(args)) {
+    throw new Error(`${args} should be an array`);
+  }
+
+  context.fn = this;
+  context.fn(...args);
+
+  delete context.fn;
+};
+
+// Bind Polyfill
+Function.prototype.myBind = function (context = {}, args = []) {
+  if (typeof this !== "function") {
+    throw new Error(`${this} should be a function`);
+  }
+
+  if (!Array.isArray(args)) {
+    throw new Error(`${args} should be an array`);
+  }
+
+  context.fn = this;
+  return function () {
+    return context.fn(...args);
+  };
+};
+
+printName.myCall(obj, 25, "Gurugram");
+printName.myApply(obj, [25, "Gurugram"]);
+const bindFunc = printName.myBind(obj, [25, "Gurugram"]);
+bindFunc();
