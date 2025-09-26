c2adb96b1a40471c89a1d08e18a1497ab81608d9
Samir Singh <samir@antino.com>
2025-09-26 22:00:57 +0530

    revised todo app in javascript

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
-let obj = {
-  name: "John",
-};
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".todo-input");
+const todoBtn = document.querySelector(".todo-btn");
+const allTodo = document.querySelector(".all-todo");
 
-function printName(age, city) {
-  console.log(`Hello my name is ${this.name} ${age} yrs old from ${city}`);
+let selectedElement = null;
+todoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (todoInput.value.trim() === "") {
+    alert("Please enter something");
+    return;
+  }
+
+  if (todoBtn.innerText === "Submit") {
+    addTodo(todoInput.value);
+  } else {
+    selectedElement.firstChild.innerText = todoInput.value;
+    todoBtn.innerText = "Submit";
+  }
+
+  todoInput.value = "";
+});
+
+allTodo.addEventListener("click", function (e) {
+  console.log(e);
+  if (e.target.tagName === "BUTTON") {
+    if (e.target.textContent === "✏️") {
+      selectedElement = e.target.parentNode;
+      todoInput.value = e.target.parentNode.firstChild.innerText;
+      todoInput.focus();
+      todoBtn.innerText = "Update";
+    } else {
+      e.target.parentNode.remove();
+    }
+  }
+});
+
+function addTodo(value) {
+  const li = document.createElement("li");
+  li.innerHTML = `<span>${value}</span>`;
+
+  const edit = document.createElement("button");
+  edit.innerText = "✏️";
+  li.appendChild(edit);
+
+  const remove = document.createElement("button");
+  remove.innerText = "❌";
+  li.appendChild(remove);
+
+  allTodo.appendChild(li);
 }
-
-// Call Polyfill
-Function.prototype.myCall = function (context = {}, ...args) {
-  if (typeof this !== "function") {
-    throw new Error(`${this} should be a function`);
-  }
-
-  context.fn = this;
-
-  context.fn(...args);
-
-  delete context.fn;
-};
-
-// Apply Polyfill
-Function.prototype.myApply = function (context = {}, args = []) {
-  if (typeof this !== "function") {
-    throw new Error(`${this} should be a function`);
-  }
-
-  if (!Array.isArray(args)) {
-    throw new Error(`${args} should be an array`);
-  }
-
-  context.fn = this;
-  context.fn(...args);
-
-  delete context.fn;
-};
-
-// Bind Polyfill
-Function.prototype.myBind = function (context = {}, args = []) {
-  if (typeof this !== "function") {
-    throw new Error(`${this} should be a function`);
-  }
-
-  if (!Array.isArray(args)) {
-    throw new Error(`${args} should be an array`);
-  }
-
-  context.fn = this;
-  return function () {
-    return context.fn(...args);
-  };
-};
-
-printName.myCall(obj, 25, "Gurugram");
-printName.myApply(obj, [25, "Gurugram"]);
-const bindFunc = printName.myBind(obj, [25, "Gurugram"]);
-bindFunc();
