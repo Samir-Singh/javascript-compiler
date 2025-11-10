0e310c81d94970be908b3d80e093b7f84f64956d
Samir Singh <samirsingh2000.ss@gmail.com>
2025-11-10 17:20:24 +0530

    accordion component revised

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
-const tabConfig = [
+const accordionConfig = [
   {
     id: 1,
-    title: "Tab 1",
+    title: "Accordion One",
     content: `
-      <h1>Tab One Heading</h1>
-      <p>Tab One Paragraph</p>
+      <h1>I am Accordion One Heading</h1>
+      <p>I am Accordion One Para</p>
     `,
   },
 
   {
     id: 2,
-    title: "Tab 2",
+    title: "Accordion Two",
     content: `
-      <h1>Tab Two Heading</h1>
-      <p>Tab Two Paragraph</p>
+      <h1>I am Accordion Two Heading</h1>
+      <p>I am Accordion Two Para</p>
     `,
   },
 
   {
     id: 3,
-    title: "Tab 3",
+    title: "Accordion Three",
     content: `
-      <h1>Tab Three Heading</h1>
-      <p>Tab Three Paragraph</p>
+      <h1>I am Accordion Three Heading</h1>
+      <p>I am Accordion Three Para</p>
     `,
   },
 ];
 
-let selectedTabId = tabConfig[0].id;
+const accordionContainer = document.querySelector(".accordion-container");
+let selectedItem = accordionConfig[0].id;
 
-const tabContainer = document.querySelector(".tab-container");
-const tabHead = document.querySelector(".tab-head");
-const tabBody = document.querySelector(".tab-body");
+accordionConfig.forEach((item) => {
+  const accordionItem = document.createElement("div");
+  accordionItem.classList.add("accordion-item");
+  accordionItem.setAttribute("aria-expanded", false);
+  accordionItem.setAttribute("data-acc-id", item.id);
 
-tabConfig.forEach((item) => {
-  const tabBtn = document.createElement("button");
-  tabBtn.setAttribute("data-tab-id", item.id);
-  tabBtn.classList.add("tab-btn");
-  tabBtn.innerText = item.title;
+  const accordionBtn = document.createElement("button");
+  accordionBtn.classList.add("accordion-btn");
+  accordionBtn.innerText = item.title;
 
-  const tabContent = document.createElement("div");
-  tabContent.innerHTML = item.content;
-  tabContent.setAttribute("data-tab-id", item.id);
-  tabContent.classList.add("tab-content");
+  const accordionContent = document.createElement("div");
+  accordionContent.innerHTML = item.content;
+  accordionContent.classList.add("accordion-content");
 
-  tabHead.appendChild(tabBtn);
-  tabBody.appendChild(tabContent);
+  accordionItem.appendChild(accordionBtn);
+  accordionItem.appendChild(accordionContent);
+  accordionContainer.appendChild(accordionItem);
 });
 
-tabContainer.addEventListener("click", function (event) {
-  if (event.target.tagName === "BUTTON") {
-    selectedTabId = event.target.getAttribute("data-tab-id");
-    activeTab(selectedTabId);
+accordionContainer.addEventListener("click", function (event) {
+  if (event.target.nodeName === "BUTTON") {
+    selectedItem = event.target.parentNode.getAttribute("data-acc-id");
+    console.log(selectedItem);
+    activeAccordion(selectedItem);
   }
 });
 
-function activeTab(id) {
-  const tabBtns = document.querySelectorAll(".tab-btn");
-  tabBtns.forEach((btn) => {
-    if (btn.getAttribute("data-tab-id") === id) {
-      btn.classList.add("active-btn");
+function activeAccordion(id) {
+  const accordionContents = document.querySelectorAll(".accordion-content");
+  accordionContents.forEach((item) => {
+    if (item.parentNode.getAttribute("data-acc-id") === id) {
+      if (item.parentNode.getAttribute("aria-expanded") === "true") {
+        item.parentNode.setAttribute("aria-expanded", false);
+        item.style.display = "none";
+      } else {
+        item.parentNode.setAttribute("aria-expanded", true);
+        item.style.display = "block";
+      }
     } else {
-      btn.classList.remove("active-btn");
-    }
-  });
-
-  const tabContents = document.querySelectorAll(".tab-content");
-  tabContents.forEach((content) => {
-    if (content.getAttribute("data-tab-id") === id) {
-      content.style.display = "block";
-    } else {
-      content.style.display = "none";
+      item.style.display = "none";
+      item.parentNode.setAttribute("aria-expanded", false);
     }
   });
 }
 
-activeTab(String(selectedTabId));
+activeAccordion(String(selectedItem));
